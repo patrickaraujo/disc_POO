41cb2365d2d6fd228a40a65128d03602efa90a6a
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-22 23:26:58 -0300

    Create exercicio5.java

---
+++ Aula01/Bloco3/exercicios/codigo-fonte/exercicio5.java
@@ -0,0 +1,6 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo Java!");
+        System.out.println("Meu primeiro programa funcionando!");
+    }
+}
