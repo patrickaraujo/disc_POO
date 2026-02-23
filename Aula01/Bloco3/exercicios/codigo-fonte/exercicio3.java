0abeb160643c853807f2248616f5e83fd83a38be
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-22 23:25:33 -0300

    Create exercicio3.java

---
+++ Aula01/Bloco3/exercicios/codigo-fonte/exercicio3.java
@@ -0,0 +1,9 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao curso de Java!");
+        System.out.println("Este é o primeiro bloco de exercícios.");
+        System.out.println("Vamos aprender os fundamentos básicos.");
+        System.out.println("Pratique bastante para ganhar fluência.");
+        System.out.println("Boa sorte em sua jornada!");
+    }
+}
