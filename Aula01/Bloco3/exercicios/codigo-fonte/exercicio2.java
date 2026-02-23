01ed06ebd0155d2ba59408b6685b4405b127c3d1
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-22 23:25:04 -0300

    Create exercicio2.java

---
+++ Aula01/Bloco3/exercicios/codigo-fonte/exercicio2.java
@@ -0,0 +1,7 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Início do programa");
+        System.out.println("Executando...");
+        System.out.println("Final do programa");
+    }
+}
