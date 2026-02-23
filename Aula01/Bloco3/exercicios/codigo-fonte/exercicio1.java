604beaae915b7885f973dd75ee096374be1e1459
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-22 23:24:40 -0300

    Create exercicio1.java

---
+++ Aula01/Bloco3/exercicios/codigo-fonte/exercicio1.java
@@ -0,0 +1,8 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        System.out.println("João Silva");
+        System.out.println("Análise e Desenvolvimento de Sistemas");
+        System.out.println("UNISA");
+        System.out.println("Estou animado para aprender Java!");
+    }
+}
