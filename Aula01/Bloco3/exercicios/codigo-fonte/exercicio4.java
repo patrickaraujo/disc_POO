8aef95e8411be4bd572e79a6308c6a009e8c8c70
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-22 23:26:24 -0300

    Create exercicio4.java

---
+++ Aula01/Bloco3/exercicios/codigo-fonte/exercicio4.java
@@ -0,0 +1,8 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        // Código com erro proposital - falta ponto e vírgula
+        System.out.println("Testando erro proposital")
+        // Para corrigir, adicione ponto e vírgula no final da linha acima
+        System.out.println("Erro corrigido!");
+    }
+}
